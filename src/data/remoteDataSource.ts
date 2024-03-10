1f244255ea31246780f57c76d9201ed793c0bdbb
Rich Lee <richalee@pm.me>
2024-03-10 13:04:23 +0000

    completed list, details etc 17.3

---
+++ src/data/remoteDataSource.ts
@@ -0,0 +1,35 @@
+import {AbstractDataSource} from "./abstractDataSource";
+import {Product} from "./entities";
+import axios from "axios";
+
+
+const protocol = "http";
+const hostname = "localhost";
+const port = 4600;
+
+const urls = {
+    products: `${protocol}://${hostname}:${port}/products`,
+    orders: `${protocol}://${hostname}:${port}/orders`
+};
+
+export class RemoteDataSource extends AbstractDataSource {
+
+    loadProducts(): Promise<Product[]> {
+        return axios.get(urls.products).then(response => response.data);
+    }
+
+    storeOrder(): Promise<number> {
+        let orderData = {
+            lines: [...this.order.orderLines.values()].map(ol =>
+                ({
+                    productId: ol.product.id,
+                    productName: ol.product.name,
+                    quantity: ol.quantity
+                }))
+        }
+        return axios.post(urls.orders, orderData)
+            .then(response => response.data.id);
+    }
+
+
+}
